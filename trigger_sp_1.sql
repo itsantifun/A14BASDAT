87b45d4de258a0dfa925c469b2780631357f4a6c
Unknown <alvanjuwanda@gmail.com>
2016-11-25 23:21:41 +0700

    tahap 2 final

--- trigger_sp_1.sql
+++ trigger_sp_1.sql
@@ -3,8 +3,15 @@ $$
 begin
 UPDATE mata_kuliah_spesial
 SET issiapsidang = true
-WHERE (OLD.ijinmajusidang <> NEW.ijinmajusidang AND OLD.pengumpulanhardcopy = true) OR (OLD.pengumpulanhardcopy <> NEW.pengumpulanhardcopy AND OLD.ijinmajusidang = true);
+WHERE (OLD.ijinmajusidang <> NEW.ijinmajusidang AND OLD.pengumpulanhardcopy = true) OR 
+	(OLD.pengumpulanhardcopy <> NEW.pengumpulanhardcopy AND OLD.ijinmajusidang = true);
 RETURN NEW;
 end;
 $$
 LANGUAGE "plpgsql" VOLATILE;
+
+create trigger upd_issiapsidang
+after update
+on MATA_KULIAH_SPESIAL
+for each row
+execute procedure upd_issiapsidang();
